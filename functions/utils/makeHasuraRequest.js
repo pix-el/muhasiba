f38bf7cbfa337cdd906fc49f47e969c6f6e1b0fb
Amin Shaikh <amin.sk77@gmail.com>
2021-10-10 22:27:02 +0100

    cleanup redirect logic a bit

--- functions/utils/makeHasuraRequest.js
+++ functions/utils/makeHasuraRequest.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
 
 exports.makeHasuraRequest = ({ query, variables }) => {
-  return fetch(process.env.HASURA_API_URL, {
+  return fetch(process.env.HASURA_GRAPHQL_API_URL, {
     method: 'POST',
     headers: { 
       Accept: 'application/json',
-      'X-Hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+      'X-Hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
       'X-Hasura-role': process.env.HASURA_USER_ROLE,
       'X-Hasura-user-id': process.env.HASURA_USER_ID,
     },
