5b262956472ac3af3f98941b9b6098d04a1cd2e6
Amin Shaikh <amin.sk77@gmail.com>
2021-10-10 22:57:05 +0100

    add basic app bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 import Container from '@mui/material/Container';
+import AppBar from './AppBar';
 import Nav from './Nav';
 import ChildRoutes from './ChildRoutes';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <AppBar />
         <Nav />
         <Container maxWidth="sm">
           <ChildRoutes />
